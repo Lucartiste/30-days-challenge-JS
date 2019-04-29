7a6183fd2ed03cc3a08435b2e316b5b3a861c752
Lucartiste <lucartiste68@gmail.com>
2019-04-29 22:17:49 +0100

    Day 1

---
+++ day 1/main.js
@@ -0,0 +1,25 @@
+function play(e){
+		const audio = document.querySelector('audio[data-key="'+e.keyCode+'"]')
+		const key = document.querySelector('.key[data-key="'+e.keyCode+'"]')
+		if (!audio) {
+			return;
+		}
+		audio.currentTime = 0;
+		audio.play();
+		key.classList.add('playing');		
+	}
+
+function removetransition(e){
+	if (e.propertyName !== 'transform') {
+		return;
+	}
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(function(e){
+	e.addEventListener('transitionend', removetransition)
+})
+
+window.addEventListener('keydown', play)
+
